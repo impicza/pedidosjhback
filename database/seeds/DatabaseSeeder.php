69a59f84772fe4644925f5c337ae7d66efe25d35
Esteban <aux.dinamica@gmail.com>
2019-09-30 15:06:12 -0500

    datos iniciales

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       
+
+    }
+}
